bf384bc7e1f0d70cd039fe722225ec5d7cab3c7b
Hendi Santika <hendisantika@gmail.com>
2017-10-04 05:35:29 +0700

    #7 - Added Address Rest Controller

---
+++ src/main/java/com/hendisantika/addressbook/web/AddressRestController.java
@@ -0,0 +1,66 @@
+package com.hendisantika.addressbook.web;
+
+import com.hendisantika.addressbook.domain.Address;
+import com.hendisantika.addressbook.repository.AddressRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : address-book-spring-boot-security-angularjs
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/30/17
+ * Time: 7:20 AM
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequestMapping(value = "/api")
+public class AddressRestController {
+	@Autowired
+	private AddressRepository addressRepository;
+
+	@RequestMapping(value = "/address", method = RequestMethod.GET)
+	public List<Address> address() {
+		return addressRepository.findAll();
+	}
+
+	@RequestMapping(value = "/address/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Address> addressById(@PathVariable Long id) {
+		Address address = addressRepository.findOne(id);
+		if (address == null) {
+			return new ResponseEntity<Address>(HttpStatus.NO_CONTENT);
+		} else {
+			return new ResponseEntity<Address>(address, HttpStatus.OK);
+		}
+
+	}
+
+	@RequestMapping(value = "/address/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Address> deleteAddress(@PathVariable Long id) {
+		Address address = addressRepository.findOne(id);
+		if (address == null) {
+			return new ResponseEntity<Address>(HttpStatus.NO_CONTENT);
+		} else {
+			addressRepository.delete(address);
+			return new ResponseEntity<Address>(address, HttpStatus.OK);
+		}
+
+	}
+
+	@RequestMapping(value = "/address", method = RequestMethod.POST)
+	public ResponseEntity<Address> createAddress(@RequestBody Address address) {
+		return new ResponseEntity<Address>(addressRepository.save(address), HttpStatus.CREATED);
+	}
+
+	@RequestMapping(value = "/address", method = RequestMethod.PUT)
+	public Address updateAddress(@RequestBody Address address) {
+		return addressRepository.save(address);
+	}
+
+}
