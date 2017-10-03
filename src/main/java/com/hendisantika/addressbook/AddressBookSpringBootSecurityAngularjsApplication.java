a26ec45ec599c58cf573bd52fb8df5e7cc5534f2
Hendi Santika <hendisantika@gmail.com>
2017-10-03 07:03:58 +0700

    #2 - Added main app

---
+++ src/main/java/com/hendisantika/addressbook/AddressBookSpringBootSecurityAngularjsApplication.java
@@ -0,0 +1,22 @@
+package com.hendisantika.addressbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : address-book-spring-boot-security-angularjs
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/30/17
+ * Time: 5:52 AM
+ * To change this template use File | Settings | File Templates.
+ */
+@SpringBootApplication
+public class AddressBookSpringBootSecurityAngularjsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressBookSpringBootSecurityAngularjsApplication.class, args);
+	}
+}
