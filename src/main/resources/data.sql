df5dc055e93c58a1d17024410bb878afd866609d
Hendi Santika <hendisantika@gmail.com>
2017-10-05 05:25:30 +0700

    #12 - Added data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into app_user(id,name,username,password,role) values (1,'Hendi','admin','admin','ADMIN');
+insert into app_user(id,name,username,password,role) values (2,'Kakashi','user','user','USER');
+
+insert into address(name,address,owner) values ('Mr. Harry W. Jones','Administrative Officer, Department of State, 4150 Sydney Place, Washington, DC 20521-4150',1);
+insert into address(name,address,owner) values ('Ms. Cynthia Ervin','Counselor of Embassy for Administrative Affairs, Department of State, 4150 Sydney Place, Washington, DC 20521-4150, DC 20521-4150',1);
+insert into address(name,address,owner) values ('Mr. William J. Bryant','American Consul, Department of State, 6170 Peshwar Place, Washington, DC 20521-6170',2);
